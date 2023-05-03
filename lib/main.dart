4e3478ca9d36a54b7db4f27a75c05f7c2431f488
kariru-k <karirukeith@students.uonbi.ac.ke>
2023-05-03 09:47:47 +0300

    Added firebase core and initialized application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pigalukudelivery/screens/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
