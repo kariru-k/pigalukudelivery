1f769fd52b744595a64a8db91c5a5accacfb55ca
kariru-k <karirukeith@students.uonbi.ac.ke>
2023-05-03 11:35:52 +0300

    Add project dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pigalukudelivery/providers/auth_provider.dart';
 import 'package:pigalukudelivery/screens/home_screen.dart';
 import 'package:pigalukudelivery/screens/login_screen.dart';
 import 'package:pigalukudelivery/screens/reset_password_screen.dart';
 import 'package:pigalukudelivery/screens/splash_screen.dart';
+import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(MultiProvider(
+      providers:[
+        ChangeNotifierProvider(create: (_) => AuthProvider())
+      ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
