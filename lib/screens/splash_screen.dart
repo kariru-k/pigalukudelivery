deaed5f22dd22f3985f258e69544f96d381c3513
kariru-k <karirukeith@students.uonbi.ac.ke>
2023-05-03 09:31:06 +0300

    Initialize firebase in application

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  static const String id = 'splash-screen';
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Timer(
+    //     const Duration(
+    //         seconds: 3
+    //     ), () {
+    //   FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    //     if(user==null){
+    //       Navigator.pushReplacementNamed(context, LoginScreen.id);
+    //     }else{
+    //       Navigator.pushReplacementNamed(context, HomeScreen.id);
+    //     }
+    //   });
+    // }
+    // );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('images/pigaluku_logo.png'),
+              const SizedBox(height: 20,),
+              const Text(
+                'Piga Luku Delivery Application',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              )
+            ],
+          )
+      ),
+    );
+  }
+}
